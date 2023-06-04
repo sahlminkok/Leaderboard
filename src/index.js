23f77f2a548c148d8a9cc24b37335bf30feb76ff
sahlminkok <sahlminkok7@gmail.com>
2023-06-04 14:25:23 +0100

    Update the MIT.md file

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import { submitScore, reset } from './modules/submitScore.js';
 import fetchScores from './modules/fetchScores.js';
 
 const gameId = 'lP0Y18KQGTeYLwKbVwzm';
-
 const form = document.querySelector('.form');
-
 const refresh = document.querySelector('.recent-scores-refresh');
+
 refresh.addEventListener('click', () => {
   fetchScores(gameId);
 });
